cb66f1037d36b862196da85084eb369d265d0f55
quyen-luc <quyen.luc@saigontechnology.com>
2022-08-26 14:43:40 +0700

    project init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  loadChildren: () => loadRemoteModule({
+    remoteName: 'shell',
+    exposedModule: './AppModule',
+    type: 'manifest'
+  }).then(m =>  m.AppModule)
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
